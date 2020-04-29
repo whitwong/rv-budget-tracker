e18a1f7d450cb01996d69bb52233ef10fba6b0fa
Whitney Wong <whitney.wong@utexas.edu>
2020-04-29 18:53:39 -0500

    Removed test button in App.jsx to ExpensePage.jsx.

--- client/src/pages/ExpensePage.jsx
+++ client/src/pages/ExpensePage.jsx
@@ -2,10 +2,26 @@ import React from 'react';
 
 // Container for Expense page view
 const ExpensePage = () => {
+
+  // Simple function to send request to /ping route and return response
+  const buttonPress = () => {
+    console.log("button pressed")
+    fetch('/ping')
+  }  
+
   return(
     <div className="ExpensePage">
       <p>Put expense data here</p>
       <p>Probably put <code>ExpenseList.jsx</code> here</p>
+
+      {/* Random button to quick test that backend is still sending back responses */}
+      <button 
+          className="click-me-button"
+          onClick={buttonPress}
+        >
+          Click Me!
+      </button>
+
     </div>
   );
 }
