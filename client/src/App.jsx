dd1e1a5ca7ea4a7ec0bb86de3b174b8e6860aa37
Whitney Wong <whitney.wong@utexas.edu>
2020-04-29 18:52:17 -0500

    Added material ui navigation bar component and removed majority boilerplate create-react-app tags.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,60 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import ExpensePage from './pages/ExpensePage';
-import AboutUsPage from './pages/AboutUs';
-import logo from './logo.svg';
+import NavigationBar from './components/BottomNavigation';
 import './App.css';
 
 // Export App to render to DOM
-const App = () => {
-
-  const buttonPress = () => {
-    console.log("button pressed")
-    fetch('/ping')
-  }
-
+const App = () => { 
   return (
     <div className="App">
-      <header className="App-header">      
-      <Router>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/ExpensePage">Expenses</Link>
-          </li>
-          <li>
-            <Link to="/AboutUsPage">About Us</Link>
-          </li>
-        </ul>
+      <header className="App-header">  
 
-        <hr />
-        <Switch>
-          <Route exact path='/'><LandingPage/></Route>
-          <Route exact path='/ExpensePage'><ExpensePage/></Route>
-          <Route exact path='/AboutUsPage'><AboutUsPage/></Route>
-        </Switch>
-      </Router>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi Whitney, Welcome to your RV Budget Tracker Dashboard
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button 
-          className="click-me-button"
-          onClick={buttonPress}
-        >
-          Click Me!
-        </button>
+        <h1>Hi Whitney <span role="img">👋</span></h1>
+        <p>Welcome to your RV Budget Tracker Dashboard</p>    
+
+        <NavigationBar />
+
       </header>
     </div>
   );
