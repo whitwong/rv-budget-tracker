5e18f5f703b18b575b3a50ebdfeaab77263f2d8c
Whitney Wong <whitney.wong@utexas.edu>
2020-04-29 17:20:07 -0500

    Added react-router-dom package to project. Cleaned up/minor additions to server index.js. Trying out react router for different pages. Added preliminary components and pages for future development. Pushing react-router-dom changes and verify that it will work on Heroku.

---
+++ client/src/pages/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Container for About Us page view
+const AboutUsPage = () => {
+  return(
+    <div className="AboutUsPage">
+      <p>Put About Us info here</p>
+    </div>
+  );
+}
+
+export default AboutUsPage;
---
+++ client/src/pages/ExpensePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Container for Expense page view
+const ExpensePage = () => {
+  return(
+    <div className="ExpensePage">
+      <p>Put expense data here</p>
+      <p>Probably put <code>ExpenseList.jsx</code> here</p>
+    </div>
+  );
+}
+
+export default ExpensePage;
