4aaa14c316e5d2c6b5b3580516f1f2feff5c529c
Whitney Wong <whitney.wong@utexas.edu>
2020-05-15 12:30:04 -0500

    Added expenses.js file which has functions for querying postgresql db (dev db for now) and returing data. Added config.js to hold config variables for project.

---
+++ server/api/expenses.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const connectionString = require('../config').connectionString;
+const { Client } = require('pg');
+
+// *** Connect to Database *** //
+const client = new Client({
+  connectionString: connectionString,
+})
+client.connect()
+
+// GET route to find and return all distinct categories
+function getCategories(req, res) {
+  client
+    .query('SELECT distinct category from expenses;')
+    .then(data => {
+      res.status(200).send(data.rows);
+    })
+    .catch(err => {
+      res.send(404).send(err);
+    })
+};
+
+// GET route to find and return all expenses of a specified category
+function getExpense(req, res) {
+  client
+    .query('SELECT * from expenses where category = $1', [req.params.category])
+    .then(data => {
+      res.status(200).send(data.rows);
+    })
+    .catch(err => {
+      res.send(404).send(err)
+    }) 
+}
+
+
+module.exports = {
+  getCategories,
+  getExpense
+};
---
+++ server/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  connectionString: process.env.DBCONN || 'postgres://whitneywong@localhost:5432/rv',
+  url: process.env.URI || 'http://localhost:3001',
+};
