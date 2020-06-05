2b989040770cb1ee883ac6dc9189f6118dd91b89
Whitney Wong <whitney.wong@utexas.edu>
2020-06-05 18:27:13 -0500

    Changed list div to material ui Card component. Initial styling.

--- client/src/components/ExpenseList.jsx
+++ client/src/components/ExpenseList.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing(50),
+      height: theme.spacing(16),
+    },
+  },
+}));
+
+// Get list data and display using Material-UI components
 const ExpenseCardList = ({ list }) => {
+  const classes = useStyles();
+
   return(
-    <div className="category-list">
+    <div className={classes.root}>
     {
       list &&
       list.map((listItem, index) => (
-        <div 
-          className="category-item"
+        <Card 
+          className="list-item"
           key={index}
           index={index}
         >
           {listItem}
-        </div>
+        </Card>
       ))
     }
   </div>
