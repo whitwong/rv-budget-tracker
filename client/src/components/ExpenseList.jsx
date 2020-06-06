62d5eb3032132534ceb4b6e9b4da81a59f2e4d82
Whitney Wong <whitney.wong@utexas.edu>
2020-06-06 00:07:07 -0500

    Added button function to query for expense details based on listItem and return data from db. String manipulation for displaying category names.

--- client/src/components/ExpenseList.jsx
+++ client/src/components/ExpenseList.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
     '& > *': {
       margin: theme.spacing(1),
       width: theme.spacing(50),
@@ -15,8 +19,24 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 // Get list data and display using Material-UI components
-const ExpenseCardList = ({ list }) => {
+const ExpenseCardList = ({ list, search }) => {
   const classes = useStyles();
+
+  // Send request to get data associated with category/month
+  const getExpenseDetails = ( listItem ) => {
+    console.log("getting expense deets " + listItem + " " + search);
+    if(search === 'category') {
+      fetch('/getExpenseCategory/'+listItem)
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }
+    else if(search === "month") {
+      console.log("getting expense deets " + listItem)
+      fetch('/getExpenseMonthly/'+listItem)
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }
+  }
 
   return(
     <div className={classes.root}>
@@ -27,8 +47,22 @@ const ExpenseCardList = ({ list }) => {
           className="list-item"
           key={index}
           index={index}
+          item={listItem}
         >
-          {listItem}
+          {/* If listItem contains '_', then replace it with '/'. 
+              Else if listItem contains and multiple uppercase characters, add space and trim space before/after string. 
+              Else return listItem. */}
+          {
+            listItem.match('_') ? listItem.replace('_', '/') : listItem.match(/([A-Z])/g).length > 1 ? listItem.replace(/([A-Z])/g, ` $1`).trim() : listItem
+          }
+          <CardActions>
+            <Button 
+              item={listItem}
+              onClick={() => getExpenseDetails(listItem, search)}
+            >
+              Details
+            </Button>
+          </CardActions>
         </Card>
       ))
     }
