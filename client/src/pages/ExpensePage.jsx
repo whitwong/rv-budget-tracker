f915c242053938f626510eb66da8903c10eef9b1
Whitney Wong <whitney.wong@utexas.edu>
2020-06-08 13:41:44 -0500

    Added new state to show category items so that getCategories did not have to query db for every request. Just show categoryList state that is already populated with initial query.

--- client/src/pages/ExpensePage.jsx
+++ client/src/pages/ExpensePage.jsx
@@ -9,19 +9,37 @@ const ExpensePage = () => {
   let [categoryList, setCategories] = useState(null);
   const [monthlyList, setMonth] = useState(null);
   const [search, setSearch] = useState(null);
+  const [showList, setShowList] = useState({
+    showCategory: false,
+    showMonthly: false
+  })
 
   // Send request to get categories from db and set categoryList with returned data. If error, throw error and log to console on client.
   const getCategories = () => {
-    console.log("retrieving categories")
-    fetch('/getCategories')
+    if (categoryList === null) {
+      fetch('/getCategories')
       .then(response => response.json())
       .then(data => { 
+        console.log("retrieving categories from db")
+
         categoryList = data.map(obj => obj.category);
         setCategories(categoryList);
         setSearch("category");
-        setMonth(null);
+        setShowList({
+          showCategory: true,
+          showMonthly: false
+        })
       })
       .catch(err => { console.log(err); throw(err) })
+    }
+    else {
+      console.log("retrieving categories from state")
+
+      setShowList({
+        showCategory: true,
+        showMonthly: false
+      })
+    }
   }  
 
   // When monthly-btn selected, set monthly list.
@@ -29,19 +47,21 @@ const ExpensePage = () => {
     console.log("retrieving months");
     setMonth(["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]);
     setSearch("month")
-    setCategories(null);
+    setShowList({
+      showCategory: false,
+      showMonthly: true
+    })
   }
 
   return(
     <div className="ExpensePage">
-      <p>Choose how you'd like to see Expense Data</p>
+      <h2>Choose how you'd like to see Expense Data</h2>
 
       {/* If categoryList or monthlyList are populated then display the chosen list */}
       {
-        categoryList || monthlyList ? 
-        <ExpenseCardList list={categoryList || monthlyList} search={search} />
-        : 
-        null
+        showList.showCategory ? <ExpenseCardList list={categoryList} search={search} /> 
+        : showList.showMonthly ? <ExpenseCardList list={monthlyList} search={search} />
+        : null
       }
 
       {/* Button to get categories of data */}
