c985f02622a48d268c5f321aa36c1de0833f4931
Whitney Wong <whitney.wong@utexas.edu>
2020-06-08 19:50:02 -0500

    Initial code to display line chart for data.

---
+++ client/src/components/ExpenseDetails.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
+
+
+const ExpenseDetails = ({ data }) => {
+  return (
+    <LineChart
+      width={1000}
+      height={700}
+      data={data}
+      margin={{
+        top: 20, right: 40, left: 80, bottom: 100,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="purchase_date" angle={-30} textAnchor="end" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="cost" stroke="#8884d8" activeDot={{ r: 8 }} />
+    </LineChart>
+  );
+}
+
+export default ExpenseDetails;
