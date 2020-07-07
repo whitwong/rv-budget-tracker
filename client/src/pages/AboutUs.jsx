6335a96d34d51b979879ffbb7bef14ef57ce19f6
Whitney Wong <whitney.wong@utexas.edu>
2020-07-07 14:58:05 -0500

    Initial update to About Us page.

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -4,7 +4,20 @@ import React from 'react';
 const AboutUsPage = () => {
   return(
     <div className="AboutUsPage">
-      <p>Put About Us info here</p>
+      <h3>About the Project</h3>
+      <p>This is a personal project. I wanted to keep up with my coding skills by creating
+        a mini dashboard showing all the expenses we had while traveling. Originally, it was
+        meant to serve as a way 1) for Dusty and I to keep up with how much we owed each other
+        and 2) to quickly see how much we spent while actively traveling on the road.
+      </p>
+      <h3>About Us</h3>
+      <p>My husband (Dusty) and I (Whitney) made 2019 into a travel year. We bought
+        a 24ft Class C RV and traveled the continental United States and bordering regions 
+        in Canada. We, of course, brought our dog Sophie with us. She was not a fan of the 
+        RV, but she did enjoy exploring all the destinations we stayed at. We hit 32 states 
+        (mostly in the west/midwest region) and visited Toronto/Niagra Falls, Calgary/Banff
+        National Park, and Vancouver/Richmond areas in Canada. 
+      </p>
     </div>
   );
 }
