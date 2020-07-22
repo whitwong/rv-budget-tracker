cbca236db62eb7aec2cefe1415ff1ac67a43d265
Whitney Wong <whitney.wong@utexas.edu>
2020-07-22 17:01:36 -0500

    Added page styling to LandingPage.

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  pageStyle: {
+    width: '80%',
+    flex: '1 0 auto'
+  },
+}))
 
 // Container for Landing Page view
 const LandingPage = () => {
+  const classes = useStyles();
+
   return(
-    <div className="LandingPage">
+    <div className={classes.pageStyle}>
 
       <h1>Hello There! <span role="img" aria-label="wave-hand" >👋</span></h1>
       <h3>Welcome to the RV Budget Dashboard for Random Ventures</h3>  
