030888ff7721e5b51a6f7577bfb6039477bcf907
Whitney Wong <whitney.wong@utexas.edu>
2020-07-31 21:27:29 -0500

    Minor styling changes. Added variables for label strings to make <text> values more readable.

--- client/src/components/LandingPagePieChart.jsx
+++ client/src/components/LandingPagePieChart.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { PieChart, Pie, Cell, } from 'recharts';
 
-const COLORS = ['#BE375F', '#ED8554', '#429fa3',];
+const COLORS = ['#BE375F', '#ED8554', '#429fA3',];
 
 const RADIAN = Math.PI / 180;
 const renderCustomizedLabel = (props) => {
   const { cx, cy, midAngle, innerRadius, outerRadius, percent, payload } = props;
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.6;
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
   const x = cx + radius * Math.cos(-midAngle * RADIAN);
   const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  const p = (percent * 100).toFixed(0);
+  const name = payload.category.match('_') ? payload.category.replace('_', '/') : payload.category.match(/([A-Z])/g).length > 1 ? payload.category.replace(/([A-Z])/g, ` $1`).trim() : payload.category;
 
   return (
-    <text x={x} y={y} fill="white" dominantBaseline="central" fontSize={'60%'}>
+    <text x={x} cx={cx} y={y} cy={cy} fill="white" textAnchor={x > cx ? 'start' : 'middle'} dominantBaseline="central" fontSize={'55%'}>
     {/* <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central"> */}
-      {`${(percent * 100).toFixed(0)}% ${payload.category.match('_') ? payload.category.replace('_', '/') : payload.category.match(/([A-Z])/g).length > 1 ? payload.category.replace(/([A-Z])/g, ` $1`).trim() : payload.category}`}
+      {`${p}% ${name}`}
     </text>
   );
 }
@@ -27,7 +29,7 @@ export default function LandingPagePieChart({ totalsList }) {
         cy={200}
         labelLine={false}
         label={renderCustomizedLabel}
-        outerRadius={150}
+        outerRadius={160}
         fill="#8884d8"
         dataKey="total"
       >
